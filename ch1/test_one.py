74a08071d85b86d6363b2f34b1ecb1ef1f36f3bf
panicoro <panicoro770@gmail.com>
2024-07-16 22:47:22 -0300

    Adding exercises

--- ch1/test_one.py
+++ ch1/test_one.py
@@ -1,2 +1,11 @@
 def test_passing():
   assert (1, 2, 3) == (1, 2, 3)
+
+def test_passing_1():
+  assert 1 in [1, 2, 3]
+
+def test_passing_2():
+  assert 2 < 5
+
+def test_passing_3():
+  assert "fizz" in "fizzbuzz"
--- ch1/test_two.py
+++ ch1/test_two.py
@@ -1,3 +1,11 @@
 def test_falling():
   assert (1, 2, 3) == (3, 2, 1)
-  
+
+def test_passing_1():
+  assert 1 in [2, 3, 4, 5]
+
+def test_passing_2():
+  assert 2 < 1
+
+def test_passing_3():
+  assert "fizz" not in "fizzbuzz"
