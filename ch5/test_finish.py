71b8507a79063ca141a0fc7a2d30c17fb630e76e
panicoro <panicoro770@gmail.com>
2024-07-31 13:17:39 -0300

    Adding chapter 5

---
+++ ch5/test_finish.py
@@ -0,0 +1,32 @@
+from cards import Card
+
+def test_finish_from_in_prog(cards_db):
+  index = cards_db.add_card(Card("second edition", state="in prog"))
+  cards_db.finish(index)
+  card = cards_db.get_card(index)
+  assert card.state == "done"
+
+def test_finish_from_done(cards_db):
+  index = cards_db.add_card(Card("write a book", state="done"))
+  cards_db.finish(index)
+  card = cards_db.get_card(index)
+  assert card.state == "done"
+  
+def test_finish_from_todo(cards_db):
+  index = cards_db.add_card(Card("create a course", state="todo"))
+  cards_db.finish(index)
+  card = cards_db.get_card(index)
+  assert card.state == "done"
+
+def test_finish(cards_db):
+  for c in [
+    Card("write a book", state="done"),
+    Card("second edition", state="in prog"),
+    Card("create a course", state="todo"),
+  ]:
+    index = cards_db.add_card(c)
+    cards_db.finish(index)
+    card = cards_db.get_card(index)
+    assert card.state == "done"
+
+  
