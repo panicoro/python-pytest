c41fca8b1a03eed3d9bcd7dd64e8f28896a39bdd
panicoro <panicoro770@gmail.com>
2024-07-30 21:00:17 -0300

    Adding ch4

---
+++ ch4/test_tmp.py
@@ -0,0 +1,10 @@
+def test_temp_path(tmp_path):
+  file = tmp_path / "file.txt"
+  file.write_text("Hello")
+  assert file.read_text() == "Hello"
+  
+def test_temp_path_factory(tmp_path_factory):
+  path = tmp_path_factory.mktemp("sub")
+  file = path / "file.txt"
+  file.write_text("Hello")
+  assert  file.read_text() == "Hello"
---
+++ ch4/test_version.py
@@ -0,0 +1,23 @@
+import cards
+import subprocess
+from typer.testing import CliRunner
+
+def test_version_v1():
+  process = subprocess.run(
+    ["cards", "version"], capture_output=True, text=True,
+  )
+  output = process.stdout.rstrip()
+  assert output == cards.__version__
+
+
+def test_version_v2(capsys):
+  cards.cli.version()
+  output = capsys.readouterr().out.rstrip()
+  assert output == cards.__version__
+
+
+def test_version_v3():
+  runner = CliRunner()
+  result = runner.invoke(cards.app, ["version"])
+  output = result.output.rstrip()
+  assert output == cards.__version__
